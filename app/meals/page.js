1aafce4c3011abcdf6d0bd83986dc2af594a8dfc
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-09-06 20:18:58 +0330

    making meals browse page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,26 @@
 import Link from "next/link";
-
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 const MealsPage = () => {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.heighlight}> by you</span>
+        </h1>
+
+        <p>
+          Choose your favorite recepie and cook it yourself! it is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 export default MealsPage;
