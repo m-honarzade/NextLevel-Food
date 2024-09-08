64b6a153dc08b331b0e19d23dcc7aeadf0434cf4
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-09-08 12:29:19 +0330

    adding loading page

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+import classes from "./loading.module.css";
+
+const LoadingMealsPage = () => {
+  return <p className={classes.loading}>Fetching Meals....</p>;
+};
+
+export default LoadingMealsPage;
