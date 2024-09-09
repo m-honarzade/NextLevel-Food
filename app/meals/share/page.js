c58907df930654c7f0b8e6558eafb961a68b8c0d
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-09-09 20:00:35 +0330

    using server action to submit form data

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,18 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 const ShareMealPage = () => {
+  const shareMeal = async (formData) => {
+    "use server";
+    const meal = {
+      title: formData.get("title"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+    };
+    console.log(meal);
+  };
   return (
     <>
       <header className={classes.header}>
@@ -10,7 +22,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
