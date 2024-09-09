bb2ab9ab226e0e25832332cffb5544b9818b6adb
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-09-09 21:34:51 +0330

    storing action in a seprate file

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,18 +1,7 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
 const ShareMealPage = () => {
-  const shareMeal = async (formData) => {
-    "use server";
-    const meal = {
-      title: formData.get("title"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-    };
-    console.log(meal);
-  };
   return (
     <>
       <header className={classes.header}>
