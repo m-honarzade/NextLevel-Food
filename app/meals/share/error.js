543c877a2856702c7d0d1ce331327fa3a16c76d8
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-09-13 13:22:18 +0330

    validating form inputs and managing error for invalid inputs using action server respones and useFormState

---
+++ app/meals/share/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>An Error occured!</h1>
+      <p>Faild to create Meal. Please try again later...</p>
+    </main>
+  );
+};
+
+export default Error;
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,12 @@
+"use client";
 import ImagePicker from "@/components/meals/image-picker";
+import { useFormState } from "react-dom";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meal-form-submit";
 
 const ShareMealPage = () => {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +16,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +45,7 @@ const ShareMealPage = () => {
             ></textarea>
           </p>
           <ImagePicker label={"userImage"} name={"image"} />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
